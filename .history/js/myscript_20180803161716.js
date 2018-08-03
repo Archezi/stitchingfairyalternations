469963554806d425b02cca500cf6497ef0e1e9ab
Lukasz Kaczmarek <luka.kaczmarek@gmail.com>
2018-08-03 21:01:27 +0100

    links corrected

--- .history/js/myscript_20180803161716.js
+++ .history/js/myscript_20180803161716.js
@@ -64,7 +64,17 @@ $(document).ready(function(){
 	});
 	
 
-	
+	// Wow Animations
+    wow = new WOW(
+		{
+		boxClass:     'wow',      // default
+		animateClass: 'animated', // default
+		offset:       0,          // default
+		mobile:       true,       // default
+		live:         true        // default
+	  }
+	  )
+	  wow.init();
 
 
 	
